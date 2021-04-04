83101972a80e180aa4de49a4cad508eb4194f0cb
matheus lima <matheu9832@gmail.com>
2021-04-04 16:28:23 -0300

    feat: add frontend base

---
+++ frontend/src/routes.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from "react-router-dom"
+
+import Dashboard from './pages/Dashboard'
+import UpdateDeveloper from './pages/UpdateDeveloper'
+import CreateDeveloper from './pages/CreateDeveloper'
+
+function Routes(){
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/updatedeveloper/:id" component={UpdateDeveloper} />
+                <Route path="/craetedeveloper" component={CreateDeveloper} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes
