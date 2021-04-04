c262912753a632dfb8eeaf4e3a31b76563433151
matheus lima <matheu9832@gmail.com>
2021-04-04 15:57:42 -0300

    chore: add project base

---
+++ backend/src/routes.js
@@ -0,0 +1,10 @@
+const routes = require("express").Router()
+const DeveloperController = require("./app/controllers/DeveloperController")
+
+routes.get('/developers', DeveloperController.index)
+routes.get('/developers/:id', DeveloperController.show)
+routes.post('/developers', DeveloperController.store)
+routes.put('/developers/:id', DeveloperController.update)
+routes.delete('/developers/:id', DeveloperController.destroy)
+
+module.exports = routes
